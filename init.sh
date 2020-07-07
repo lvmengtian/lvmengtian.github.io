a5d11d15e5accc5e75e73be156b367349b52f54f
lvmengtian <lvmengtian@taou.com>
2020-07-07 16:23:50 +0800

    添加init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+echo 'begin to install dependencies'
+npm install hexo --save
+npm install hexo-deployer-git --save
+npm install hexo-generator-archive --save
+npm install hexo-generator-category --save
+npm install hexo-generator-index --save
+npm install hexo-generator-tag --save
+npm install hexo-renderer-ejs --save
+npm install hexo-renderer-marked --save
+npm install hexo-renderer-stylus --save
+npm install hexo-server --save
+echo 'install dependencies success'
